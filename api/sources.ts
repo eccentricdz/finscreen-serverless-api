759ffe12e1ccd0f2a23409b0dfda4ea9c1bc74cb
Rahul Agarwal <rahulaga@amazon.com>
2021-05-30 16:50:40 +0530

    Merge DAO and its types with /api/sources

--- api/sources.ts
+++ api/sources.ts
@@ -1,10 +1,49 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
-import { MongoClient } from "mongodb";
-import SourcesDAO from "./dao/SourcesDao";
-import Sources from "./types/Source";
+import { Collection, Cursor, Db, MongoClient } from "mongodb";
+import { ObjectId } from "bson";
+
+interface Source {
+    name: String;
+    url: String;
+    colorOne: String;
+    colorTwo: String;
+    _id: ObjectId;
+}
+
+let finscreenDB: Db;
+let sources: Collection<Source>;
+
+class SourcesDAO {
+    static async injectDB(client: MongoClient) {
+        try {
+            finscreenDB = await client.db(process.env.FINSREEN_DB_NS);
+            sources = await finscreenDB.collection("sources");
+        } catch (e) {
+            console.error(
+                `Unable to establish a collection handle in sourcesDAO: ${e}`
+            );
+        }
+    }
+
+    /**
+     * Get all the sources in the collection
+     * @returns Promise<Source[]>
+     */
+    static async getSources(): Promise<Source[]> {
+        let cursor: Cursor<Source>;
+        try {
+            cursor = await sources.find();
+            return cursor.toArray();
+        } catch (e) {
+            console.error(`Unable to find sources in the Collection: ${e}`);
+        }
+    }
+}
 
 async function establishDbConnection() {
-    await MongoClient.connect(process.env.FINSCREEN_DB_URI, { useNewUrlParser: true })
+    await MongoClient.connect(process.env.FINSCREEN_DB_URI, {
+        useNewUrlParser: true,
+    })
         .catch((err) => {
             console.error(err.stack);
             process.exit(1);
--- api/types/Source.ts
+++
@@ -1,9 +0,0 @@
-import { ObjectId } from "bson";
-
-export default interface Sources {
-    name: String;
-    url: String;
-    colorOne: String;
-    colorTwo: String;
-    _id: ObjectId;
-}
