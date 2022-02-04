cdca1acf589c20801125a5416eb95fa540c72a16
Rahul Agarwal <rahulaga@amazon.com>
2022-02-04 12:18:08 -0800

    Actually add articles DAO.

---
+++ api/articles/[sourceId].ts
@@ -0,0 +1,118 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import { Collection, Cursor, Db, MongoClient } from "mongodb";
+import * as XMLParser from "fast-xml-parser";
+import fetch from 'node-fetch';
+import { ObjectId } from "bson";
+
+interface Source {
+    name: string;
+    url: string;
+    colorOne: string;
+    colorTwo: string;
+    _id: ObjectId;
+}
+
+interface Article { 
+    title: string;
+    link: string;
+    date: string
+}
+
+let finscreenDB: Db;
+let sources: Collection<Source>;
+
+class ArticlesDAO {
+    static async getArticles(source: Source): Promise<any> {
+        try {
+            const response = await fetch(source.url);
+            if (!response.ok) {
+                throw new Error(
+                    `Network response was not ok: ${response.statusText}`
+                );
+            }
+
+            const responseXML = await response.text();
+            let responseJSON = {};
+
+            if (XMLParser.validate(responseXML) === true) {
+                responseJSON = XMLParser.parse(responseXML);
+                return responseJSON;
+            } else {
+                throw new Error(`Error when trying to parse the XML response`);
+            }
+        } catch (error) {
+            console.error(
+                "There has been a problem with your fetch operation:",
+                error
+            );
+        }
+    }
+}
+
+class SourceDAO {
+    static async injectDB(client: MongoClient) {
+        try {
+            finscreenDB = await client.db(process.env.FINSREEN_DB_NS);
+            sources = await finscreenDB.collection("sources");
+        } catch (e) {
+            console.error(
+                `Unable to establish a collection handle in sourcesDAO: ${e}`
+            );
+        }
+    }
+
+    /**
+     * Get all the sources in the collection
+     * @returns Promise<Source[]>
+     */
+    static async getSource(id: ObjectId): Promise<Source[]> {
+        let cursor: Cursor<Source>;
+        try {
+            cursor = await sources.find({ _id: id });
+            return cursor.limit(1).toArray();
+        } catch (e) {
+            console.error(`Unable to find sources in the Collection: ${e}`);
+        }
+    }
+}
+
+async function establishDbConnection() {
+    await MongoClient.connect(process.env.FINSCREEN_DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+        .catch((err) => {
+            console.error(err.stack);
+            process.exit(1);
+        })
+        .then(async (client) => {
+            await SourceDAO.injectDB(client);
+        });
+}
+
+/**
+ * We need to create a new deployment everytime we add a new source to the DB
+ * This will make sure our cache is invalidated and our users get the latest
+ * set of sources.
+ */
+const setCacheHeaders = (response: VercelResponse): VercelResponse => {
+    response.setHeader("Cache-Control", "max-age=0, s-maxage=900");
+    return response;
+};
+
+/**
+ * The primary entry point for the API /api/sources
+ */
+export default async (request: VercelRequest, response: VercelResponse) => {
+    try {
+        const sourceId: string = request.query.sourceId as string;
+        await establishDbConnection();
+        const sources = await SourceDAO.getSource(new ObjectId(sourceId));
+        const articles = await ArticlesDAO.getArticles(sources[0]);
+        return setCacheHeaders(response).status(200).json(articles);
+    } catch (error) {
+        console.error(
+            `Unable to fetch the sources from the SourceDAO : ${error}`
+        );
+    }
+};
